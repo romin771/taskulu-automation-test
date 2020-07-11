cf4eea47ada1643a6ffeeb400d76c562483afb9e
Romin parvardeh <romin.parvardeh@gmail.com>
2020-07-11 17:12:49 +0430

    Custom Selenium Webdriver Added

--- pages/home/ijad_proje_shakhsi__page.py
+++ pages/home/ijad_proje_shakhsi__page.py
@@ -1,16 +1,22 @@
 from selenium.webdriver.common.by import By
 import time
+from base.mySeleniumDriver import mySeleniumDriver
 
-class iJad_felan_page():
+
+                    #inherit from this class which our custom class
+class iJad_felan_page(mySeleniumDriver):
     # its a normal pythonn class so we should define constructor
     #there are few thing i want to provide to this class  to work on the element
 
     def __init__(self, driver):
+        super().__init__(driver)
           #provide driver instance - chon hame karaye find element click element ke marbod be driver hast ro mikhaym inja ham anjam bedim
         self.driver = driver
 
 
-    #locator
+    #1)locator
+    # here it does not matter if its xpath, name , id ... its just locator
+    #when ever any locator changes on website, next release or any time, we should just change here. thats it ;)
     _emailText_Field = "//input[@placeholder='Email, e.g. bilbo@example.com']"
     _passwordText_Field = "//div[@class='form-group']//input[@placeholder='Password, e.g. •••••••••••••••••']"
     _login_Button = "//button[contains(text(),'LOGIN')]"
@@ -18,9 +24,16 @@ class iJad_felan_page():
     _ijadProje = "//h4[contains(text(),'پروژه‌های شخصی')]//following-sibling::button"
     _onvanProje ="//input[@placeholder='عنوان پروژه']"
     _besazButton = "//button[@class='btn raised primary pull-right']"
+    _ijadTodo = "//div[@class='list-tasks-wrapper _view_element']//div[1]//div[1]//i[1]"
 
+    #2)define method to expose locators into element ( find element ...  )
     #expose upper locators in to web element by defining function
     #this element then can be utelize in any number of methond
+    """
+    Note 1 ) since we creat a function clicking on element and sending data to it in our 
+    custom selenium class ( mySeleniumDriver.py) + also we are finding those element 
+    inside our framework classes , we can get rid of this lines :
+    
     def getEmailTextField(self):
         return self.driver.find_element(By.XPATH, self._emailText_Field)
     def getPasswordTextField(self):
@@ -35,14 +48,48 @@ class iJad_felan_page():
         return self.driver.find_element(By.XPATH, self._onvanProje)
     def getbesazButton(self):
         return self.driver.find_element(By.XPATH, self._besazButton)
+    def getijadTODO(self):
+        return self.driver.find_element(By.XPATH, self._ijadTodo)"""
 
 
 
 
+    """ based on Note1 which explained above, we can get rid of commented lines 
+    
+      def enterEmail(self, email):
+        #self.getEmailTextField().send_keys(email)
+        
+        #self.getPasswordTextField().send_keys(passw)
+        #self.getLoginButton().click()
+        #self.getBadanButton().click()
+    """
+
+    #3)actions on elements // CALL THESE FROM OUR CUSTOM SELENIUM WEBDRIVER
+    def enterEmail(self, email):
+        self.sendKeys(email, self._emailText_Field, locatorType="xpath")
+    def enterPassword(self, passw):
+        self.sendKeys(passw,self._passwordText_Field, locatorType="xpath")
+    def clickLoginButton(self):
+        self.elementClick(self._login_Button, locatorType="xpath")
+    def clickBadanButton(self):
+        self.elementClick(self._badan_button, locatorType="xpath")
+
+    #main functionality // it wrapp the actions together for doing A function
+    def login2(self, email, password):
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
+        self.clickBadanButton()
+
+
+
+    #instead of these codes we can come up with uppers  : after creating :
+    #1)locators - 2)method to expose - 3)action on methods
 
     def login(self, username, password):
         emailTextField = self.driver.find_element(By.XPATH, "//input[@placeholder='Email, e.g. bilbo@example.com']")
         emailTextField.send_keys(username)
+
 
         passwordTextField = self.driver.find_element(By.XPATH,
                                                      "//div[@class='form-group']//input[@placeholder='Password, e.g. •••••••••••••••••']")
@@ -57,7 +104,14 @@ class iJad_felan_page():
         ijadProje.click()
         time.sleep(2)
         onvanProje = self.driver.find_element(By.XPATH, "//input[@placeholder='عنوان پروژه']")
+
         onvanProje.send_keys("TestForRomin")
 
         besazButton = self.driver.find_element(By.XPATH, "//button[@class='btn raised primary pull-right']")
         besazButton.click()
+
+        ijadTodo = self.driver.find_element(By.XPATH, "//div[@class='list-tasks-wrapper _view_element']//div[1]//div[1]//i[1]")
+        ijadTodo.click()
+
+
+
